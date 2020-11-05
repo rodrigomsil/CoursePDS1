bc6acece256bea960c5c4ca42dfe2519985bc241
Rodrigo <rodrigo.miranda@estudante.iftm.edu.br>
2020-11-05 09:55:55 -0300

    Project created

---
+++ src/main/java/com/iftm/PDS1/Pds1Application.java
@@ -0,0 +1,13 @@
+package com.iftm.PDS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pds1Application.class, args);
+	}
+
+}
