02a71fea8e4af49bf3683160ea6d97017e67a207
Rodrigo <rodrigo.miranda@estudante.iftm.edu.br>
2020-12-08 21:38:16 -0300

    OrderDTO

--- src/main/java/com/iftm/PDS1/services/OrderService.java
+++ src/main/java/com/iftm/PDS1/services/OrderService.java
@@ -2,12 +2,15 @@ package com.iftm.PDS1.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.iftm.PDS1.dto.OrderDTO;
 import com.iftm.PDS1.entities.Order;
 import com.iftm.PDS1.repositories.OrderRepository;
+import com.iftm.PDS1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
@@ -15,12 +18,15 @@ public class OrderService {
 	@Autowired
 	private OrderRepository  repository;
 
-	public List<Order> findAll() {
-		return repository.findAll();
+	
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findAll();
+		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
 	}
 	
-	public Order findById(Long id) {
+	public OrderDTO findById(Long id) {
 		Optional<Order> obj =  repository.findById(id);
-		return obj.get();
+		Order entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		return new OrderDTO(entity);
 	}
 }
