78c0fd27da7ac39a6e9b8d1c7a518bb8e17ce72e
Rodrigo <rodrigo.miranda@estudante.iftm.edu.br>
2020-12-08 22:18:24 -0300

    Product insert

--- src/main/java/com/iftm/PDS1/dto/ProductCategoriesDTO.java
+++ src/main/java/com/iftm/PDS1/dto/ProductCategoriesDTO.java
@@ -1,45 +1,37 @@
 package com.iftm.PDS1.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.iftm.PDS1.entities.Product;
 
-public class ProductDTO implements Serializable {
+public class ProductCategoriesDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Long id;
 	private String nome;
 	private String description;
 	private Double price;
 	private String imgUrl;
 	
-	public ProductDTO() {
+	List<CategoryDTO> categories = new ArrayList<>();
+	
+	public ProductCategoriesDTO() {
 		
 	}
 
-	public ProductDTO(Long id, String nome, String description, Double price, String imgUrl) {
-		super();
-		this.id = id;
+	public ProductCategoriesDTO(String nome, String description, Double price, String imgUrl) {
 		this.nome = nome;
 		this.description = description;
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
 	
-	public ProductDTO(Product entity) {
-		this.id = entity.getId();
-		this.nome = entity.getNome();
-		this.description = entity.getDescription();
-		this.price = entity.getPrice();
-		this.imgUrl = entity.getImgUrl();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
+	public ProductCategoriesDTO(Product entity) {
+		setNome(entity.getNome());
+		setDescription(entity.getDescription());
+		setPrice(entity.getPrice());
+		setImgUrl(entity.getImgUrl());
 	}
 
 	public String getNome() {
@@ -73,9 +65,19 @@ public class ProductDTO implements Serializable {
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-	
-	public  Product toEntity() {
-		return new Product(id, nome, description, price, imgUrl);
+
+	public List<CategoryDTO> getCategories() {
+		return categories;
 	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	public Product toEntity() {
+		return new Product(null, nome, description, price, imgUrl);
+	}
+	
 	
 }
