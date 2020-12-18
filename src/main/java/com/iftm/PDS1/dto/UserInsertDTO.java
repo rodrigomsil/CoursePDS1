720692bf8a78b2c17f6cfa218aeeaf1e9fad91a1
Rodrigo <rodrigo.miranda@estudante.iftm.edu.br>
2020-12-18 13:38:37 -0300

    Custom user insert validation: unique email

--- src/main/java/com/iftm/PDS1/dto/UserInsertDTO.java
+++ src/main/java/com/iftm/PDS1/dto/UserInsertDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.iftm.PDS1.entities.User;
+import com.iftm.PDS1.services.validation.UserInsertValid;
 
+@UserInsertValid
 public class UserInsertDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 
---
+++ src/main/java/com/iftm/PDS1/services/validation/UserInsertValidator.java
@@ -0,0 +1,42 @@
+package com.iftm.PDS1.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.iftm.PDS1.dto.UserInsertDTO;
+import com.iftm.PDS1.entities.User;
+import com.iftm.PDS1.repositories.UserRepository;
+import com.iftm.PDS1.resources.exceptions.FieldMessage;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User user = userRepository.findByEmail(dto.getEmail());
+		if (user != null) {
+			list.add(new FieldMessage("email", "email already taken"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
